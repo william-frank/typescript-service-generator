5f5e1efe741daaa27f737d7f2b594478b0f5ef2b
William Frank <info@williamfrank.net>
2026-01-15 17:02:18 -0500

    Update copyrights

--- src/test/java/org/omega/typescript/processor/MethodDefinitionBuilderTest.java
+++ src/test/java/org/omega/typescript/processor/MethodDefinitionBuilderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 William Frank (info@williamfrank.net)
+ * Copyright (c) 2018-2026 William Frank (info@williamfrank.net)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
